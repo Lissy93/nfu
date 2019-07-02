bc9c3be323edb4626995dc7d397a6134c2b70495
Alicia Sykes <alicia.sykes@protonmail.com>
2019-07-02 18:28:53 +0100

    Wrote initial DayCount Model

--- lib/models/DayCountModel.dart
+++ lib/models/DayCountModel.dart
@@ -1,7 +1,31 @@
+import 'dart:convert';
+
+DayCount clientFromJson(String str) {
+  final jsonData = json.decode(str);
+  return DayCount.fromMap(jsonData);
+}
+
+String clientToJson(DayCount data) {
+  final dyn = data.toMap();
+  return json.encode(dyn);
+}
+
 class DayCount {
   int id;
   String title;
   int date;
 
   DayCount({this.id, this.title, this.date});
+
+  factory DayCount.fromMap(Map<String, dynamic> json) => new DayCount(
+    id: json["id"],
+    title: json["title"],
+    date: json["date"],
+  );
+
+  Map<String, dynamic> toMap() => {
+    "id": id,
+    "title": title,
+    "date": date,
+  };
 }
